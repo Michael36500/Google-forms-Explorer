f9effb1a6bcc184cfdd7e7668ac91e8b82b7f1c1
Michael36500 <104157791+Michael36500@users.noreply.github.com>
2022-06-21 19:43:58 +0200

    threading, found.txt, better output

--- test4.py
+++ test4.py
@@ -19,7 +19,7 @@ def forms(iteration):
         found = open("found.txt", "w")
         # print(a)
         if a % 1000 in range(0, 20):
-            print("thread {} on {}k".format(iteration, a//1000), end=" ")
+            print("thread {} on {}k".format(iteration, a//1000))
             # print(a, end=" ")
         web = str(list[a%62]) + str(list[a//62%62]) + str(list[a//3844%62]) + str(list[a//238328%62]) + str(list[a//14776336%62]) + str(list[a//916132832%62]) + str(list[a//56800235584%62]) + str(list[a//3521614606208%62]) + str(list[a//218340105584896%62]) + str(list[a//13537086546263552%62]) + str(list[a//839299365868340224%62]) + str(list[a//520365606838370938888%62]) + str(list[a//3226266762397899821056%62]) + str(list[a//200028539268669788905472%62]) + str(list[a//12401769434657526912139264%62]) + str(list[a//768909704948766668552634368%62]) + str(list[a//47672401706823533450263330816%62])
 
