9c2323daa802bb4e8a770db5098ea4b130874a55
Michael36500 <104157791+Michael36500@users.noreply.github.com>
2022-06-26 17:59:05 +0200

    nothing

--- test4.py
+++ test4.py
@@ -24,7 +24,7 @@ def forms(iteration):
             # print(a, end=" ")
         web = str(list[a%62]) + str(list[a//62%62]) + str(list[a//3844%62]) + str(list[a//238328%62]) + str(list[a//14776336%62]) + str(list[a//916132832%62]) + str(list[a//56800235584%62]) + str(list[a//3521614606208%62]) + str(list[a//218340105584896%62]) + str(list[a//13537086546263552%62]) + str(list[a//839299365868340224%62]) + str(list[a//520365606838370938888%62]) + str(list[a//3226266762397899821056%62]) + str(list[a//200028539268669788905472%62]) + str(list[a//12401769434657526912139264%62]) + str(list[a//768909704948766668552634368%62]) + str(list[a//47672401706823533450263330816%62])
 
-        response = requests.get('https://forms.gle/{}'.format(web))
+        response = requests.get('https://forms.gle/{}'.format(web), timeout=0.01)
         # time.sleep(10)
         # print(iteration, web)
         if response.status_code == 200:
